e22ee10176772ec46495ded70a86276740dc0e5c
NamagiriLakshmiMVL <namagirimvl@gmail.com>
2024-02-08 11:44:26 +0530

    done

--- src/pages/DisplayMsg.jsx
+++ src/pages/DisplayMsg.jsx
@@ -1,47 +1,37 @@
 import { Box, Checkbox, Typography } from '@mui/material'
 import axios from 'axios';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { API } from '../API';
 
 function DisplayMsg() {
-
+    const [message, setMessage] = useState([])
     const items = JSON.parse(localStorage.getItem('email'));
-   
-    axios.post(`${API}/gmail//getting-msg`,items)
-    .then((res)=>console.log(res))
-  
+    const result = {
+        items
+    }
+    useEffect(() => {
+        axios.post(`${API}/gmail//getting-msg`, result)
+            .then((res) => setMessage(res.data))
+    }, [])
 
+
+    console.log(message)
     return (
         <div>
-            {/* {email.map((msg) => {
+            {message.map((details) => {
                 return (
-                    <div style={{display:"flex"}}>
-
-                        <Checkbox size='small' />
-                        <Box>
-                            <Typography style={{ width: 200 }}>{x.firstname}</Typography>
-                            <Typography style={{ width: 200 }}>{msg}</Typography>
-                            
-                          
-                        </Box>
-                       
+                    <div>
+                        <h1>{details.from}</h1>
+                        <h1>{details.to}</h1>
+                        <h1>{details.message}</h1>
                     </div>
                 )
 
-            }
-            )} */}
- </div >
+            })}
+
+        </div >
     )
 }
 export default DisplayMsg
 
-  // const data = useSelector((state) => state.loginSlice.loginDetails)
-    // const arr = []
-    // arr.push(data[data.length-1])
-    // const x = arr[0]
-    // const email = []
-    // for (let i = 0; i < x.message.length; i++) {
-    //     email.push(x.message[i])
-    // }
-    // console.log(x)
