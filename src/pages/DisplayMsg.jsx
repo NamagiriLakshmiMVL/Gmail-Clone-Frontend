d9a5bb7b8b896c34634fe27ae85592bbf3bc861d
NamagiriLakshmiMVL <namagirimvl@gmail.com>
2024-02-08 20:38:23 +0530

    done

--- src/pages/DisplayMsg.jsx
+++ src/pages/DisplayMsg.jsx
@@ -1,13 +1,13 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { API } from '../API';
-import DeleteOutlineTwoToneIcon from '@mui/icons-material/DeleteOutlineTwoTone';
-import { Checkbox, Tooltip, Typography } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/DeleteOutlineTwoTone';
+import { Button, Checkbox, Tooltip, Typography } from '@mui/material';
 import StarBorder from '@mui/icons-material/StarBorder';
 import ArrowBackTwoToneIcon from '@mui/icons-material/ArrowBackTwoTone';
 
 
-function DisplayMsg({send}) {
+function DisplayMsg({ send }) {
     const [message, setMessage] = useState([])
     const items = JSON.parse(localStorage.getItem('email'));
     const result = {
@@ -19,30 +19,32 @@ function DisplayMsg({send}) {
             .then((res) => setMessage(res.data))
     }, [send])
 
-   
+    const handleDelete = (id) => {
+        const newdata = {
+            id
+        }
+        axios.post(`${API}/gmail/deleting-msg`, newdata)
+            .then((res) => alert(res.data))
+    }
     return (
         <div>
-            <ArrowBackTwoToneIcon/>
+            <ArrowBackTwoToneIcon />
             {message.map((details) => {
                 return (
                     <div className='displaymsg-root'>
                         <Tooltip title={details.message}>
-                            <table className="displaymsg" onClick={()=>console.log(details._id)}>
+                            <table className="displaymsg" onClick={() => console.log(details._id)}>
                                 <Checkbox size='small' />
                                 <StarBorder fontSize='small' style={{ marginRight: 10 }} />
                                 <Typography style={{ width: 200 }}>{details.from}</Typography>
                                 <Typography style={{ width: 200 }}>{details.subject}</Typography>
                                 <Typography style={{ width: 200 }}>{details.message}</Typography>
-                                <DeleteOutlineTwoToneIcon/>
+                                <Button onClick={() => handleDelete(details._id)}><DeleteIcon color='inherit' /></Button>
                             </table>
                         </Tooltip>
-
-
                     </div>
                 )
-
             })}
-
         </div >
     )
 }
